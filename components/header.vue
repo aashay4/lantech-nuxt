49eb7233179b4cb53841fbe369a001d2c99f2814
Aashay <aashay.trivedi1@gmail.com>
2021-07-16 17:53:50 +0530

    first

--- components/header.vue
+++ components/header.vue
@@ -24,6 +24,7 @@
 </div>
 
 <!-- Sidebar on small screens when clicking the menu icon -->
+<client-only>
 <nav class="w3-sidebar w3-bar-block w3-black w3-card w3-animate-left w3-hide-medium w3-hide-large" v-show="isclose" id="mySidebar">
   <a href="javascript:void(0)" v-on:click='isclose = !isclose' class="w3-bar-item w3-button w3-large w3-padding-16">Close ×</a>
   <router-link to="/web-development-services/">
@@ -35,6 +36,7 @@
   <router-link to="/contact-us/">
   <a onclick="w3_close()" class="w3-bar-item w3-button"><i class="fa fa-phone"></i> Contact Us</a></router-link>
 </nav>
+</client-only>
 </div>
 </template>
 
