889bf5aad3f814582f7a45ed13b1fc3c5aefa9e3
Aashay <aashay.trivedi1@gmail.com>
2021-07-20 18:17:12 +0530

    first

--- pages/must-have-android-apps.vue
+++ pages/must-have-android-apps.vue
@@ -13,17 +13,22 @@
  <p>Here in this article, we have shared top android apps that you should have for an easier life. Read this article and get more knowledge. If you find any other apps that are more useful then do let us know, we would love to add the app to our list.</p>
 <h3>Airbnb</h3>
 <p>Airbnb should be installed to find the rooms, homes, apartments, or any other type of accommodation at unknown/known locations at the most affordable price. You can book both long-term booking and short-term booking on Airbnb easily. If you are traveling then this app makes it easy for you to book!</p>
+<p><b>Play store link: </b><a href="https://play.google.com/store/apps/details?id=com.airbnb.android">https://play.google.com/store/apps/details?id=com.airbnb.android</a></p>
 <h3>Weather</h3>
 <p>This app can be a blessing for travelers especially those who are on a road trip. Keeping the weather app helps you to know the weather conditions where you are traveling and if the weather is not so good then you can alter your route.</p>
+<p><b>Play store link: </b><a href="https://play.google.com/store/apps/details?id=com.accurate.live.weather.forecast.pro">https://play.google.com/store/apps/details?id=com.accurate.live.weather.forecast.pro</a></p>
 <h3>DropBox</h3>
 <p>Dropbox is an excellent cloud storage app that saves your data and you can get data no matter what device you are using. Great app if you have a lot of data and you need to use it frequently.</p>
+<p><b>Play store link: </b><a href="https://play.google.com/store/apps/details?id=com.dropbox.android">https://play.google.com/store/apps/details?id=com.dropbox.android</a></p>
 <h3>Binary translator</h3>
 <p>Binary translator app is an educational app and useful for students who are into electronics and computer science. With this app, you can convert the binary numbers into text values, decimal, hexadecimal, and octal with just one click. Great for those who have limited access to the internet because this app works on the slow internet and off-line as well. A great choice if you are a student!</p>
 <p><b>Play store link: </b><a href="https://play.google.com/store/apps/details?id=com.binary.text">https://play.google.com/store/apps/details?id=com.binary.text</a></p>
 <h3>Google Drive</h3>
 <p>If you have to deal with a lot of data then downloading Google Drive and signing up for Google drive can be a great option. It is free, along with that, you can edit and create various word and PPT documents easily with the help of this app. Not only that, but it also helps you to save all the important data on the cloud. You can use and access that data from anywhere with internet access.</p>
+<p><b>Play store link: </b><a href="https://play.google.com/store/apps/details?id=com.google.android.apps.docs">https://play.google.com/store/apps/details?id=com.google.android.apps.docs</a></p>
 <h3>Amazon</h3>
 <p>Amazon is yet another search engine for shopping. You can get a tremendous amount of products listed on Amazon. In your free time, you can visit it and get detailed knowledge about the same. Along with that, Amazon also runs an affiliate program for the users. You can go through various products, find out the best ones, create an affiliate link of the products and directly send it to the people who are looking for such products. With the help of an affiliate program, you can earn a little commission as well.</p>
+<p><b>Play store link: </b><a href="https://play.google.com/store/apps/details?id=com.amazon.mShop.android.shopping&hl=en_US&gl=US">https://play.google.com/store/apps/details?id=com.amazon.mShop.android.shopping&hl=en_US&gl=US</a></p>
 <p>So, these are the top best android apps that you need to install on your android devices. After installing these apps, if you feel, any other apps deserve to be on this list then do contact us. We would be glad to include such apps on this list.</p>
                 </div>
               </div>
