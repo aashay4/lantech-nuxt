0c7ec03fec986a03675b92e39a9c8fdc1b546d38
Aashay <aashay.trivedi1@gmail.com>
2021-07-24 16:00:25 +0530

    first

---
+++ pages/binarytotext.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div class="w3-main" style="margin-left:250px">
+      <div class="w3-row w3-padding-64">
+        <div class="w3-twothird w3-container">
+          <h1 class="w3-text-teal">Binary To Text Converter (Translator)</h1>
+<binarytext />
+          <Adsense
+        class="adsbygoogle infeed"
+        style="display:block"
+        data-ad-client=""
+        data-ad-slot=""
+        data-ad-format="auto"
+        data-full-width-responsive="true">
+        </Adsense>
+            <h1 class="w3-text-teal">Binary Translator</h1>
+            <div>
+            <binarytranslator />
+          </div>
+        </div><br>
+            </div>
+  </div>
+</div>
+</template>
+
+<script>
+import binarytext from '../components/binarytext.vue';
+import binarytranslator from '../components/binarytranslator.vue';
+
+export default {
+  head() {
+  // sets document title
+  return {
+  title: '',
+  // optional; sets final title as "Index Page - My Website", useful for multiple level meta
+  // meta tags
+  meta: [
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'robots', content: 'noindex,nofollow' }
+  ],
+  link: [
+ {rel: 'canonical', href: 'https://binarytotext.net/'}
+]
+}
+},
+    components:{
+    'binarytranslator': binarytranslator,
+    'binarytext': binarytext
+  },
+  data() {
+    return {
+      elementVisible: false
+    }
+  },
+  created() {
+           setTimeout(() => this.elementVisible = true, 5000)
+       }
+}
+</script>
+
+<style>
+#showMe {
+  animation: cssAnimation 0s 5s forwards;
+  visibility: hidden;
+}
+
+@keyframes cssAnimation {
+  to   { visibility: visible; }
+}
+@media (min-width: 350px) {
+    .infeed {
+      height: 290px;
+    }
+  }
+ @media (min-width: 500px) {
+    .infeed {
+      height: 290px;
+    }
+  }
+ @media (min-width: 800px) {
+    .infeed {
+      height: 290px;
+    }
+  }
+table, th, td {
+  border: 1px solid black;
+}
+#box {
+  background-color: lightgrey;
+   width: 300px;
+   border: 5px solid gray;
+   padding: 10%;
+   margin-left: 30%;
+
+}
+</style>
