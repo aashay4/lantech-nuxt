59d1983c703357ccd7cf40367c811f183ea3411f
Aashay <aashay.trivedi1@gmail.com>
2021-09-09 18:10:04 +0530

    first

--- api/controllers/articlesController.js
+++ api/controllers/articlesController.js
@@ -2,35 +2,6 @@ const wol = require('wol');
 const is_ip_private = require('private-ip')
 var network = require('network');
 
-module.exports.get_public_ip = function (req, res, next) {
-  network.get_public_ip(function(err, ip) {
-
-  console.log(err || ip); // should return your public IP address
-  return res.json(ip);
-})
-    };
-    module.exports.get_private_ip = function (req, res, next) {
-      network.get_private_ip(function(err, ip) {
-  console.log(err || ip); // err may be 'No active network interface found'.
-  return res.json(ip);
-})
-        };
-        module.exports.get_gateway_ip = function (req, res, next) {
-          network.get_gateway_ip(function(err, ip) {
-  console.log(err || ip); // err may be 'No active network interface found.'
-  return res.json(ip);
-})
-            };
-
-            module.exports.get_interfaces_list = function (req, res, next) {
-              network.get_interfaces_list(function(err, list) {
- console.log(list);
- return res.json(list);
-
-})
-                };
-
-
     module.exports.woll = function (req, res, next) {
       var x = req.body.input;
       var answer;
